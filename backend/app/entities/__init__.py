1913beef19249af63699f9538dd9499d9d1ec882
Konstantine Machavariani <kotiko2004@gmail.com>
2026-05-28 20:58:37 +0400

    Add backend import

--- backend/app/entities/__init__.py
+++ backend/app/entities/__init__.py
@@ -1,5 +1,5 @@
 from app.entities.picker import Picker
-from app.entities.fruit import Fruit
+from app.entities.field import Field
 from app.entities.box import Box
 from app.entities.print_batch import PrintBatch
 from app.entities.harvest_entry import HarvestEntry
